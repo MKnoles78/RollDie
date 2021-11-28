5f17036a85e010b0aefa34fee927914b5b1c2834
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 01:50:15 -0500

    added for loop to roll dice 60MM times and loop to store counts

--- src/RollDieRefactor.java
+++ src/RollDieRefactor.java
@@ -1,8 +1,25 @@
+
 //Fig. 7.7: RollDieRefactor
+//Die-rolling program using arrays instead of switch
+import java.security.SecureRandom;
+
 public class RollDieRefactor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SecureRandom randomNumbers = new SecureRandom();
+		int[] frequency = new int[7]; // array of frequency counters
+
+		// roll die 60,000,000 times; use die value as frequency index
+		for (int roll = 1; roll <= 60_000_000; roll++) {
+			++frequency[1 + randomNumbers.nextInt(6)];
+		}
+
+		System.out.printf("%s%10%s%n", "Face", "Freqency");
+
+		// output each array element's value
+		for (int face = 1; face < frequency.length; face++) {
+			System.out.printf("%4d%10%d%n", face, frequency[face]);
+		}
 
 	}
 
