129bbebc438399488da337ade9aa9bd7e10b47bd
Mike Knoles <mknoles78@gmail.com>
2021-11-27 20:18:45 -0500

    added switch case to store counts

--- src/RollDie.java
+++ src/RollDie.java
@@ -20,8 +20,29 @@ public class RollDie {
 		for (int roll = 1; roll <= 60_000_000; roll++) {
 			int face = 1 + randomNumbers.nextInt(6); // number from 1 to 6
 
+			// use face value 1-6 to determine which counter to increment
+			switch (face) {
+			case 1:
+				++frequency1; // increment the 1s counter
+				break;
+			case 2:
+				++frequency2; // increment the 2s counter
+				break;
+			case 3:
+				++frequency3; // increment the 3s counter
+				break;
+			case 4:
+				++frequency4; // increment the 4s counter
+				break;
+			case 5:
+				++frequency5; // increment the 5s counter
+				break;
+			case 6:
+				++frequency6; // increment the 6s counter
+				break;
+			}
 		}
 
+		System.out.println("Face\tFrequency"); // output headers
 	}
-
 }
